2920e958c61ed30aabd91ee73ca66f6ab6a3c19a
nomer900986 <nomer900986@yandex.ru>
2021-11-16 00:09:54 +0300

    added lab 2 and 3

---
+++ web2/firstwebpage/flatpages/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django import template
+from django.http import HttpResponse
+
+def home(request):
+
+	return render(request, 'templates/static_handler.html')
+
+# Create your views here.
